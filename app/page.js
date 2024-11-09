19e697db208e1a45e43f813e05128ba385ef2008
Rajat Bhatia <rajat.bhatia859@gmail.com>
2024-11-09 21:23:06 +0530

    check

--- app/page.js
+++ app/page.js
@@ -1,10 +1,27 @@
+'use client'
+
+import { useRouter } from 'next/navigation'
+import { useEffect } from 'react'
+import { useUser } from '@clerk/nextjs'
 
 
 
 export default function Home() {
+  const user = useUser().user
+  const router = useRouter()
+  useEffect(()=>{
+    if(user?.primaryEmailAddress?.emailAddress){
+      router.push('/dashboard')
+    }else{
+      router.push('/sign-in')
+    }
+    //comment
+    
+  },[])
+  
   return (
   <div>
-          PREP AI
+          
   </div>
   );
 }
