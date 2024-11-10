080ec52733c3f875fd90ec694b570a1e94e821ee
Rajat Bhatia <rajat.bhatia859@gmail.com>
2024-11-10 08:03:15 +0530

    checking auto pipeline

--- firebase/firebase-config.js
+++ firebase/firebase-config.js
@@ -1,17 +1,21 @@
+
+
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBFQKVSQilY4WYgSNzs-quwRqILYwKs29I",
-  authDomain: "prepai-ai.firebaseapp.com",
-  projectId: "prepai-ai",
-  storageBucket: "prepai-ai.firebasestorage.app",
-  messagingSenderId: "394539887187",
-  appId: "1:394539887187:web:675bf08de5cc6bc56434a6"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket:process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGE_ID,
+  appId: process.env.FIREBASE_APPID
 };
+console.log("firebase-config",firebaseConfig)
 
 
 
